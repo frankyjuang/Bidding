5ac74bf15e4802aa553174da8205138b3b5063fe
mark <markwoo@gmail.com>
2018-07-24 22:53:49 +0800

    update defaults

--- Bidding/Bidding/RevealedInfo.cs
+++ Bidding/Bidding/RevealedInfo.cs
@@ -6,13 +6,13 @@ namespace Bidding.Bidding
         public int minPoints { get; set; } = 0;
         public int maxPoints { get; set; } = 40;
         public int minClubs { get; set; } = 0;
-        public int maxClubs { get; set; } = 0;
+        public int maxClubs { get; set; } = 13;
         public int minDiamonds { get; set; } = 0;
-        public int maxDiamonds { get; set; } = 0;
+        public int maxDiamonds { get; set; } = 13;
         public int minHearts { get; set; } = 0;
-        public int maxHearts { get; set; } = 0;
+        public int maxHearts { get; set; } = 13;
         public int minSpades { get; set; } = 0;
-        public int maxSpades { get; set; } = 0;
+        public int maxSpades { get; set; } = 13;
         public string Note { get; set; }
     }
 }
